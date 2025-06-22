b680e998de63c7932e9636375f3ecc774cc09465
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-22 17:12:31 -0300

    Teste add agenda

---
+++ src/main/java/com/mycompany/projetolp2/Agenda.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetolp2;
+
+/**
+ *
+ * @author T-GAMER
+ */
+
+public class Agenda  {
+    private String status;
+    private String especialidade;
+    
+    public Agenda(String especialidade, String status) {
+        this.especialidade = especialidade;
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}

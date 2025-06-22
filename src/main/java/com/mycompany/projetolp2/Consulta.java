0ed9d793ef0cb9116dd78ba155ccfce11198b10f
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-22 19:22:19 -0300

    add localDate consulta

--- src/main/java/com/mycompany/projetolp2/Consulta.java
+++ src/main/java/com/mycompany/projetolp2/Consulta.java
@@ -4,18 +4,20 @@
  */
 package com.mycompany.projetolp2;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author T-GAMER
  */
 public class Consulta {
-    private String dataHora;
+    private LocalDate dataHora;
     private Animal animal;
     private boolean consultaMarcada;
     private String diagnostico, problema,medicamento;
     private Veterinario veterinario;
 
-    public Consulta(String dataHora, Animal animal, boolean consultaMarcada, String diagnostico, String problema, String medicamento, Veterinario veterinario) {
+    public Consulta(LocalDate dataHora, Animal animal, boolean consultaMarcada, String diagnostico, String problema, String medicamento, Veterinario veterinario) {
         this.dataHora = dataHora;
         this.animal = animal;
         this.consultaMarcada = consultaMarcada;
@@ -25,11 +27,11 @@ public class Consulta {
         this.veterinario = veterinario;
     }
 
-    public String getDataHora() {
+    public LocalDate getDataHora() {
         return dataHora;
     }
 
-    public void setDataHora(String dataHora) {
+    public void setDataHora(LocalDate dataHora) {
         this.dataHora = dataHora;
     }
 
@@ -80,6 +82,8 @@ public class Consulta {
     public void setVeterinario(Veterinario veterinario) {
         this.veterinario = veterinario;
     }
+
+    
     
     
 }

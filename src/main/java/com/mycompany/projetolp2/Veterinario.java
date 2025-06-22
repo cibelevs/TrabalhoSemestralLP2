107ba53dbe5df0b0c1a6b9842d2fa8f6bc3feb70
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-22 17:32:36 -0300

    add vet

---
+++ src/main/java/com/mycompany/projetolp2/Veterinario.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetolp2;
+
+//veterinários que devem ser cadastrados
+// indicando nome, cpf, email, telefone, especialidade 
+// e o número do CFMV (conselho federal de medicina veterinária 
+
+public class Veterinario extends Pessoa
+{
+    private String especialidade, numeroCfmv;
+    
+    public Veterinario(String nome, String cpf, String email, String telefone,String especialidade, String numeroCfmv)
+    {
+        super(nome, cpf, email, telefone);
+        this.especialidade = especialidade;
+        this.numeroCfmv = numeroCfmv;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+
+    public String getNumeroCfmv() {
+        return numeroCfmv;
+    }
+
+    public void setNumeroCfmv(String numeroCfmv) {
+        this.numeroCfmv = numeroCfmv;
+    }
+    
+  
+}

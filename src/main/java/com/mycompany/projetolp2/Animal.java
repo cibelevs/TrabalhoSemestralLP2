a933a5004124fef421d5ff4b6c527b21071ea4c6
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-22 17:38:36 -0300

    add class animal

---
+++ src/main/java/com/mycompany/projetolp2/Animal.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projetolp2;
+
+//import java.util.ArrayList;
+
+public class Animal
+{
+    //Todo animal atendido na clínica deve
+    //ser cadastrado como nome, raça, data de nascimento e tutor
+    private String nome, raca;
+    private String dataNascimento; //coloquei em string 
+    private Tutor tutor;
+    //private ArrayList<RegistroVacina> vacinasTomadas;
+
+    public Animal(String nome, String raca, String dataNascimento, Tutor tutor)
+    {
+        this.nome = nome;
+        this.raca = raca;
+        this.tutor = tutor;
+        this.dataNascimento =  dataNascimento;
+        
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
+
+    
+}

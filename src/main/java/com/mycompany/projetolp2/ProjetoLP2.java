d27126ee96db843c75cacd34db83316676fb0cbc
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-22 17:06:22 -0300

    Primeiro commit: Projeto Maven criado no NetBeans

---
+++ src/main/java/com/mycompany/projetolp2/ProjetoLP2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetolp2;
+
+/**
+ *
+ * @author T-GAMER
+ */
+public class ProjetoLP2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Vai dar td certo!");
+    }
+}

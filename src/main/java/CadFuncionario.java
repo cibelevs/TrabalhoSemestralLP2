0313a87987c4e082745558ce1d892b353bcf81fc
Cibele Vale <cibelevale.estudante@gmail.com>
2025-06-24 22:12:55 -0300

    fechando tela cadFuncionario

--- src/main/java/CadFuncionario.java
+++ src/main/java/CadFuncionario.java
@@ -41,7 +41,7 @@ public class CadFuncionario extends javax.swing.JFrame {
         txtFuncao = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI Historic", 1, 24)); // NOI18N
         jLabel1.setText("Cadastro de Funcionários");
